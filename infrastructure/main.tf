1f40dc588b2cf530db32356ed78520ae42ecfb5c
Joseph Cowin <ChiefSphinx@protonmail.com>
2022-08-14 00:16:32 +0200

    Initial Infra

---
+++ infrastructure/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "joecowin_rg" {
+  name     = "joecowin"
+  location = "North Europe"
+}
+
+resource "azurerm_virtual_network" "joecowin_vnet" {
+  name                = "joecowin-network"
+  resource_group_name = azurerm_resource_group.joecowin_rg.name
+  location            = azurerm_resource_group.joecowin_rg.location
+  address_space       = ["10.0.0.0/16"]
+}
