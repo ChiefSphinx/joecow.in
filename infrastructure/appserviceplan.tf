cc6beabf7674892ff12fb397c68d347cafe5a861
Joseph Cowin <ChiefSphinx@protonmail.com>
2022-08-14 01:24:16 +0200

    Add App Service

--- infrastructure/appserviceplan.tf
+++ infrastructure/appserviceplan.tf
@@ -5,3 +5,12 @@ resource "azurerm_service_plan" "joecowin_asp" {
   os_type             = "Linux"
   sku_name            = "F1"
 }
+
+resource "azurerm_linux_web_app" "joecowin_as" {
+  name                = var.name
+  resource_group_name = azurerm_resource_group.joecowin_rg.name
+  location            = azurerm_service_plan.joecowin_asp.location
+  service_plan_id     = azurerm_service_plan.joecowin_asp.id
+
+  site_config {}
+}
