e842e2d89e4d57fff8632e3e6b6342291a424582
Joseph Cowin <ChiefSphinx@protonmail.com>
2022-08-14 01:15:24 +0200

    Fix RG

--- infrastructure/appserviceplan.tf
+++ infrastructure/appserviceplan.tf
@@ -1,7 +1,7 @@
 resource "azurerm_service_plan" "joecowin_asp" {
   name                = "${var.name}-asp"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.joecowin_rg.name
+  location            = azurerm_resource_group.joecowin_rg.location
   os_type             = "Linux"
   sku_name            = "F1"
 }
