13e69d9240668ed03692b032370a361e3452cd47
Joseph Cowin <ChiefSphinx@protonmail.com>
2022-08-14 17:30:34 +0200

    Update React App

--- infrastructure/appserviceplan.tf
+++ infrastructure/appserviceplan.tf
@@ -11,6 +11,7 @@ resource "azurerm_linux_web_app" "joecowin" {
   resource_group_name = azurerm_resource_group.joecowin_rg.name
   location            = azurerm_service_plan.joecowin_asp.location
   service_plan_id     = azurerm_service_plan.joecowin_asp.id
+  https_only          = true
 
 
   site_config {
