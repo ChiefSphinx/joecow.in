5c9c4811af139e07e60d9afeb5aaf2dea32bcdca
Joseph Cowin <ChiefSphinx@protonmail.com>
2022-08-14 22:28:03 +0200

    Add Application Insights to App Settings

--- infrastructure/appserviceplan.tf
+++ infrastructure/appserviceplan.tf
@@ -12,8 +12,19 @@ resource "azurerm_linux_web_app" "joecowin" {
   location            = azurerm_service_plan.joecowin_asp.location
   service_plan_id     = azurerm_service_plan.joecowin_asp.id
   https_only          = true
-
-
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY"                  = "d64eb4f6-db68-4925-9257-42cf401fe8e3"
+    "APPINSIGHTS_PROFILERFEATURE_VERSION"             = "1.0.0"
+    "APPINSIGHTS_SNAPSHOTFEATURE_VERSION"             = "1.0.0"
+    "APPLICATIONINSIGHTS_CONNECTION_STRING"           = "InstrumentationKey=d64eb4f6-db68-4925-9257-42cf401fe8e3;IngestionEndpoint=https://northeurope-0.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/"
+    "ApplicationInsightsAgent_EXTENSION_VERSION"      = "~3"
+    "DiagnosticServices_EXTENSION_VERSION"            = "~3"
+    "InstrumentationEngine_EXTENSION_VERSION"         = "disabled"
+    "SnapshotDebugger_EXTENSION_VERSION"              = "disabled"
+    "XDT_MicrosoftApplicationInsights_BaseExtensions" = "disabled"
+    "XDT_MicrosoftApplicationInsights_Mode"           = "recommended"
+    "XDT_MicrosoftApplicationInsights_PreemptSdk"     = "disabled"
+  }
   site_config {
     always_on = false
   }
