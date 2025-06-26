cfe537a471132b4d7c6521106360d71dcd4f0aaa
Joseph Cowin <JosephCowin@hotmail.com>
2025-06-26 15:43:12 +0200

    refactor: Update Azure App Service configuration to use new resource types
    - Changed resource type from azurerm_app_service_plan to azurerm_service_plan.
    - Updated app service resource from azurerm_app_service to azurerm_linux_web_app.
    - Modified configuration to use application_stack for Docker image specification.
    - Streamlined app service plan settings for improved clarity and functionality.

--- infrastructure/appservice.tf
+++ infrastructure/appservice.tf
@@ -1,29 +1,24 @@
-resource "azurerm_app_service_plan" "main" {
+resource "azurerm_service_plan" "main" {
   name                = "asp-${var.product}-${var.environment}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type             = "Linux"
+  sku_name            = "F1"
 }
 
-resource "azurerm_app_service" "main" {
+resource "azurerm_linux_web_app" "main" {
   name                = "app-${var.product}-${var.environment}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  app_service_plan_id = azurerm_app_service_plan.main.id
+  service_plan_id     = azurerm_service_plan.main.id
 
   site_config {
-    linux_fx_version = "DOCKER|<DOCKER_IMAGE_NAME>:<TAG>"
+    application_stack {
+      docker_image_name = "chiefsphinx/joecow.in:latest"
+    }
   }
 
   app_settings = {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
   }
 }
-
-# Replace <DOCKER_IMAGE_NAME>:<TAG> with your actual Docker image (e.g., joecowin/vite-app:latest)
