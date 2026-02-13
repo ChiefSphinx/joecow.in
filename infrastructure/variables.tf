9f6c28238b34170f29322f55ae12b1151a30765a
Joseph Cowin <JosephCowin@Proton.me>
2026-02-13 19:44:15 +0000

    feat: add Cloudflare provider configuration and project variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -15,3 +15,15 @@ variable "location" {
   type        = string
   default     = "West Europe"
 }
+
+variable "project_name" {
+  description = "The name of the Cloudflare Pages project"
+  type        = string
+  default     = "joecowin"
+}
+
+variable "production_branch" {
+  description = "The production branch for the Pages project"
+  type        = string
+  default     = "main"
+}
