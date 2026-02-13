3b534eee3b793a3bd1960d858a394ad9acdfc381
Joseph Cowin <JosephCowin@Proton.me>
2026-02-13 19:44:15 +0000

    feat: add Cloudflare provider configuration and project variables

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>4.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
   }
 }
 
@@ -12,3 +16,12 @@ provider "azurerm" {
   features {}
 }
 
+provider "cloudflare" {}
+
+data "cloudflare_account" "main" {}
+
+resource "cloudflare_pages_project" "main" {
+  account_id        = data.cloudflare_account.main.id
+  name              = var.project_name
+  production_branch = var.production_branch
+}
