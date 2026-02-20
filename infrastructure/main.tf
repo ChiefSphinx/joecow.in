7c4cb1ceef3245d875069d7b9cf828b88c3dc46e
Joseph Cowin <JosephCowin@Proton.me>
2026-02-20 15:23:45 +0000

    fix: uncomment Cloudflare pages project resource configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -36,8 +36,8 @@ resource "cloudflare_zone" "joecowin" {
   name = var.zone_name
 }
 
-//resource "cloudflare_pages_project" "joecowin" {
-//  account_id        = data.cloudflare_accounts.this.result[0].id
-//  name              = var.project_name
-//  production_branch = var.production_branch
-//}
+resource "cloudflare_pages_project" "joecowin" {
+  account_id        = data.cloudflare_accounts.this.result[0].id
+  name              = var.project_name
+  production_branch = var.production_branch
+}
